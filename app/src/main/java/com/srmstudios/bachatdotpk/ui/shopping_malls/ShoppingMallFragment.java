775ed66615a95691c7e0ab6e9b7771c8077a31b2
Shahrukh <shahrukh@mobiwhiz.com>
2017-10-29 16:11:43 +0500

    checking commit

--- app/src/main/java/com/srmstudios/bachatdotpk/ui/shopping_malls/ShoppingMallFragment.java
+++ app/src/main/java/com/srmstudios/bachatdotpk/ui/shopping_malls/ShoppingMallFragment.java
@@ -24,7 +24,11 @@ public class ShoppingMallFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_mall, container, false);
+        View v = inflater.inflate(R.layout.fragment_shopping_mall, container, false);
+
+        
+
+        return v;
     }
 
 }
